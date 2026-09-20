1f93e20fe78cda7a47808f60236a6df75a783717
ejaaa18 <delzdelzganzz07@gmail.com>
2026-09-20 19:54:03 +0700

    .

--- run.py
+++ run.py
@@ -1,20 +1,20 @@
 import os
 import sys
 
-PRIVATE = ".ezraprivate"
+PRIVATE = os.path.expanduser("~/storage/downloads/otpeja")
 TARGET = "eja18.py"
 
 
 def main():
     old_dir = os.getcwd()
-    target_path = os.path.join(old_dir, PRIVATE, TARGET)
+    target_path = os.path.join(PRIVATE, TARGET)
 
     if not os.path.exists(target_path):
         print("[!] File tidak ada: " + target_path)
-        print("[!] Pastikan folder " + PRIVATE + " ada di folder ini.")
+        print("[!] Pastikan folder otpeja ada di Download.")
         sys.exit(1)
 
-    os.chdir(os.path.join(old_dir, PRIVATE))
+    os.chdir(PRIVATE)
     try:
         with open(target_path, "r", encoding="utf-8") as f:
             kode = f.read()
